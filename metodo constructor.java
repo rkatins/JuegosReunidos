851c151ceb9acc535b7fc9fc679d347d6fd2b0fa
Rubén Katins Durán <83010981+rkatins@users.noreply.github.com>
2021-05-27 19:10:03 +0200

    prueba

--- metodo constructor.java
+++ metodo constructor.java
@@ -1,30 +1,30 @@
-package Coches;
+package pqtExamen;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class CExamen_0302 {
     public  static void main(){
         Scanner woTeclado = new Scanner(System.in);
-        ArrayList<CAlumno> wlAlumno = new ArrayList<>();
         
         String wsNombre;
         int wiEdad;
         int wiAltura;
+        boolean wbTerminar = true;
         
         CAlumno poAlumno = new CAlumno(wsNombre, wiEdad, wiAltura);
-
-        System.out.println("Pulsa <INTRO> para dejar de introuducir datos");
-        for (int i = 0; i < wlAlumno.size >= i; i++){
+        
+        while (wbTerminar)
             System.out.println("Nombre del alumno: ");
-              wsNombre = woTeclado.nextLine();
+                wsNombre = woTeclado.nextLine();
             System.out.println("Edad del alumno: ");
-              wiEdad = woTeclado.nextInt(); woTeclado.nextLine();
+                wiEdad = woTeclado.nextInt(); woTeclado.nextLine();
             System.out.println("Altura del alumno: ");
-              wiAltura = woTeclado.nextInt(); woTeclado.nextLine();
-
-            while (wlAlumno.equals(""))
-                poAlumno.mvMostrarDatos();
-        } //for (int i = 0; i < wlAlumno; i++)
+                wiAltura = woTeclado.nextInt(); woTeclado.nextLine();
+              
+            if (wbTerminar = false) {
+                break;
+            }
+        } //while 
     } //main()
 } //CExamen_0302
 
@@ -39,5 +39,5 @@ class CAlumno{
 
     void mvMostrarDatos() {
         System.out.println(" \nNombre: " + psNombre + " Edad: " + piEdad+ " Altura: " +piAltura);
-    }//mvMostrarDatos
-  } //CAlumno{}
+    } //mvMostrarDatos
+} //CAlumno
