88409a6cd325b5a9f35e4744ca2eb84aed01caf0
Rubén Katins Durán <83010981+rkatins@users.noreply.github.com>
2021-12-19 20:45:03 +0100

    Añadir método ganador
    PD: Agradecimientos a Ricardo y Daniel por la ayuda con el método
    PD2: Aunque el método fue ligeramente modificado

--- TresEnRalla/Tablero.java
+++ TresEnRalla/Tablero.java
@@ -1,4 +1,4 @@
-package TresEnRalla;
+package TresEnRaya;
 
 public class Tablero {
 
@@ -22,14 +22,44 @@ public class Tablero {
         System.out.println("------------------\n");
     }
     
-    public void mComprobarGanador(char[][] tablero3EnRalla) {
-		for (int i = 0; i < tablero3EnRalla.length; i++) {
-			for (int j = 0; j < tablero3EnRalla.length; j++) {
-				if (tablero3EnRalla[i][j] == 'X') {
-					
-				}
-			}
-		}
-	}
+    public boolean comprobarGanadorX(char[][] tablero3EnRalla, Jugador[] jugador) {
+        for (int i = 0; i < tablero3EnRalla.length; i++) {
+            if (tablero3EnRalla[i][0] == 'X' && tablero3EnRalla[i][0] == tablero3EnRalla[i][1] && tablero3EnRalla[i][0] == tablero3EnRalla[i][2]) {
+            	jugador[0].ganador = true;
+            }
+        }
+        for (int i = 0; i < tablero3EnRalla.length; i++) {
+            if (tablero3EnRalla[0][i] == 'X' && tablero3EnRalla[0][i] == tablero3EnRalla[1][i] && tablero3EnRalla[0][i] == tablero3EnRalla[2][i]) {
+            	jugador[0].ganador = true;
+            }
+        }
+        //diagonales
+        if (tablero3EnRalla[0][0] == 'X' && tablero3EnRalla[0][0] == tablero3EnRalla[1][1] && tablero3EnRalla[0][0] == tablero3EnRalla[2][2]) {
+        	jugador[0].ganador = true;
+        } else if (tablero3EnRalla[0][2] == 'X' && tablero3EnRalla[0][2] == tablero3EnRalla[1][1] && tablero3EnRalla[0][2] == tablero3EnRalla[2][0]) {
+        	jugador[0].ganador = true;
+        }
+        return jugador[0].ganador;
+    }
+    
+    public boolean comprobarGanadorO(char[][] tablero3EnRalla, Jugador[] jugador) {
+        for (int i = 0; i < tablero3EnRalla.length; i++) {
+            if (tablero3EnRalla[i][0] == 'O' && tablero3EnRalla[i][0] == tablero3EnRalla[i][1] && tablero3EnRalla[i][0] == tablero3EnRalla[i][2]) {
+            	jugador[1].ganador = true;
+            }
+        }
+        for (int i = 0; i < tablero3EnRalla.length; i++) {
+            if (tablero3EnRalla[0][i] == 'O' && tablero3EnRalla[0][i] == tablero3EnRalla[1][i] && tablero3EnRalla[0][i] == tablero3EnRalla[2][i]) {
+            	jugador[1].ganador = true;
+            }
+        }
+        //diagonales
+        if (tablero3EnRalla[0][0] == 'O' && tablero3EnRalla[0][0] == tablero3EnRalla[1][1] && tablero3EnRalla[0][0] == tablero3EnRalla[2][2]) {
+        	jugador[1].ganador = true;
+        } else if (tablero3EnRalla[0][2] == 'O' && tablero3EnRalla[0][2] == tablero3EnRalla[1][1] && tablero3EnRalla[0][2] == tablero3EnRalla[2][0]) {
+        	jugador[1].ganador = true;
+        }
+        return jugador[1].ganador;
+    }
 }
 
